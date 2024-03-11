f1dd44e65ba90b243cbda39e4f79d1f6554e904c
Yimkeul <leesjun29@gmail.com>
2024-03-11 20:14:02 +0900

    upload

---
+++ Comi/Comi/ComiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComiApp.swift
+//  Comi
+//
+//  Created by yimkeul on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ComiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
