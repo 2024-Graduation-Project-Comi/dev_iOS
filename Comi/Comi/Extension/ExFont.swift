3887d0c8bec2d0c839f261477d9a246bb666faf4
Yimkeul <leesjun29@gmail.com>
2024-04-29 14:34:13 +0900

    Add Font

---
+++ Comi/Comi/Extension/ExFont.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont.swift
+//  Comi
+//
+//  Created by yimkeul on 4/29/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static let ptBold18: Font = .custom("Pretendard-Bold", size: 18)
+    static let ptSemiBold18: Font = .custom("Pretendard-SemiBold-Bold", size: 18)
+    static let ptSemiBold14: Font = .custom("Pretendard-SemiBold-Bold", size: 14)
+    static let ptRegular18: Font = .custom("Pretendard-Regular", size: 18)
+    static let ptRegular14: Font = .custom("Pretendard-Regular", size: 14)
+}
