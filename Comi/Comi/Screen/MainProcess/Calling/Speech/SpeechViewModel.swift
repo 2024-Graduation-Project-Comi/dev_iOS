d18b965f732f128022f1a0c29a0578879ef42d78
MoonGun <glass23000@naver.com>
2024-05-13 10:32:03 +0900

    Chore: Lint warning 수정

--- Comi/Comi/Screen/MainProcess/Calling/Speech/SpeechViewModel.swift
+++ Comi/Comi/Screen/MainProcess/Calling/Speech/SpeechViewModel.swift
@@ -16,37 +16,37 @@ class SpeechViewModel: ObservableObject {
     private var audioCheckInterval: TimeInterval = 0.1  // 타이머 간격
     private var maxConsecutiveNoInputTime: TimeInterval = 3  // 사용자 입력 감지 간격
     private var consecutiveNoInputCount: TimeInterval = 0  // 사용자 입력 카운터
-    
+
     @Published var isRecording = false
     @Published var isPlaying = false
     @Published var audioURL: URL?
-    
+
     func startRecrding() {
         guard !isRecording else { return }
-        
+
         if #available(iOS 17.0, *) {
             let recordingApplication = AVAudioApplication.shared
             let recordingSession = AVAudioSession.sharedInstance()
             do {
-                
+
             } catch {
-                
+
             }
         } else {
             let recordingSession = AVAudioSession.sharedInstance()
-            
+
             do {
                 try recordingSession.setCategory(.playAndRecord)
                 try recordingSession.setActive(true)
-                
+
                 recordingSession.requestRecordPermission{ [weak self] granted in
                     guard granted else {
                         print("Error: User denied recording permission.")
                         return
                     }
-                    
+
                     guard let self = self else { return }
-                    
+
                     let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
                     let audioFileName = documentPath.appendingPathComponent("recording.wav")
                     let settings = [
@@ -55,13 +55,13 @@ class SpeechViewModel: ObservableObject {
                         AVNumberOfChannelsKey: 1,
                         AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
                     ]
-                    
+
                     do {
                         self.audioRecorder = try AVAudioRecorder(url: audioFileName, settings: settings)
                         try FileManager.default.createDirectory(at: documentPath, withIntermediateDirectories: true, attributes: nil)
                         self.audioRecorder?.record()
                         self.audioRecorder?.isMeteringEnabled = true
-                        
+
                         DispatchQueue.main.async {
                             self.isRecording = true
 //                            self.startRecrdingTimer()
@@ -74,6 +74,6 @@ class SpeechViewModel: ObservableObject {
                 print("Error: Failed to set up recording session.")
             }
         }
-        
+
     }
 }
